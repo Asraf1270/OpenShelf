deda88e58c332a26cc24840ebf717e991a5c1323
Asraf1270 <mdasrafulislam1270@gmail.com>
2026-07-27 22:47:34 +0600

    refactor: modularize CSS by extracting standalone stylesheets and updating view references

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/book.css') }}">
+@endpush
+
 @section('content')
 <div class="book-detail">
 
--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/books.css') }}">
+@endpush
+
 @section('content')
 <style>
     .hide-on-mobile { display: block; }
